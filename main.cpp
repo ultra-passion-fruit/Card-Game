18aa4c07d284cc006b05e6325d66b2fc7040a5da
ultra-passion-fruit <matsuru@hoshi.com.br>
2024-11-19 21:24:07 -0500

    main function initial

--- main.cpp
+++ main.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
+#include "Player.h"
+#include "Table.h"
 
 void main() {
+    Player p1;
+    Player p2;
+    // creating factory
+    CardFactory* factory = CardFactory::getFactory();;
+
     std::cout << "Welcome to Card Game!" << std::endl;
     // Player 1 input name
+    std::cin >> p1;
     // Player 2 input name    
+    std::cin >> p1;
+    
+    std::cout << "Great! Let's begin." << std::endl;
 
-    // Draw 5 cards for P1
-    // Draw 5 cards for P2
+    // making table (deals cards in ctor)
+    Table table(p1, p2, factory);
+    
+    // winner variable for later
+    std::string winner;
 
     // OR
 
     // Load game from file
 
-    // while deck not empty
-
+    while (!table.win(winner))
+    {
         // if PAUSE then save game (to file) and exit
         // else
-
+        
         // for each player
 
-            // dispaly Table
+            // display Table
+    }
+
+        
 
 }
