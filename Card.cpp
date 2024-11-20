8e06a396404a70bcccba98f16326be4b132ed93d
Leo <zwei029@uottawa.ca>
2024-11-19 22:59:13 -0500

    Rough draft for Card class

---
+++ Card.cpp
@@ -0,0 +1,115 @@
+#include "Card.h"
+
+int Blue::getCardsPerCoin(int coins) {
+    if(coins >= 1 && coins <= 4){
+        return valTable[coins - 1];
+    } else {
+        return 0;
+    }
+}
+std::string Blue::getName() const {
+    return "Blue";
+}
+void Blue::print(std::ostream &os) {
+    os << "B";
+}
+
+int Chili::getCardsPerCoin(int coins) {
+    if(coins >= 1 && coins <= 4){
+        return valTable[coins - 1];
+    } else {
+        return 0;
+    }
+}
+std::string Chili::getName() const {
+    return "Chili";
+}
+void Chili::print(std::ostream &os) {
+    os << "C";
+}
+
+int Stink::getCardsPerCoin(int coins) {
+    if(coins >= 1 && coins <= 4){
+        return valTable[coins - 1];
+    } else {
+        return 0;
+    }
+}
+std::string Stink::getName() const {
+    return "Stink";
+}
+void Stink::print(std::ostream &os) {
+    os << "S";
+}
+
+int Green::getCardsPerCoin(int coins) {
+    if(coins >= 1 && coins <= 4){
+        return valTable[coins - 1];
+    } else {
+        return 0;
+    }
+}
+std::string Green::getName() const {
+    return "Green";
+}
+void Green::print(std::ostream &os) {
+    os << "G";
+}
+
+int Soy::getCardsPerCoin(int coins) {
+    if(coins >= 1 && coins <= 4){
+        return valTable[coins - 1];
+    } else {
+        return 0;
+    }
+}
+std::string Soy::getName() const {
+    return "soy";
+}
+void Soy::print(std::ostream &os) {
+    os << "s";
+}
+
+int Black::getCardsPerCoin(int coins) {
+    if(coins >= 1 && coins <= 4){
+        return valTable[coins - 1];
+    } else {
+        return 0;
+    }
+}
+std::string Black::getName() const {
+    return "black";
+}
+void Black::print(std::ostream &os) {
+    os << "b";
+}
+
+int Red::getCardsPerCoin(int coins) {
+    if(coins >= 1 && coins <= 4){
+        return valTable[coins - 1];
+    } else {
+        return 0;
+    }
+}
+std::string Red::getName() const {
+    return "Red";
+}
+void Red::print(std::ostream &os) {
+    os << "R";
+}
+
+int Garden::getCardsPerCoin(int coins) {
+    if(coins >= 2 && coins <= 3){
+        return valTable[coins - 1];
+    } else {
+        return 0;
+    }
+}
+std::string Garden::getName() const {
+    return "garden";
+}
+void Garden::print(std::ostream &os) {
+    os << "g";
+}
+
+
--- Card.h
+++ Card.h
@@ -10,8 +10,91 @@
 * Blue, Chili, Stink, Green, soy, black, Red and garden.
 */
 class Card {
-    public:
-        virtual int getCardsPerCoin(int) = 0;
-        virtual std::string getName() = 0;
-        virtual void print(std::ostream&) = 0;
+protected:
+    int valTable[4]{};
+public:
+    explicit Card(const int chainVal[4]){
+        for(int i = 0; i < 4; i++){
+            valTable[i] = chainVal[i];
+        }
+    }
+    Card(const Card&) = delete;
+    Card& operator=(const Card&) = delete;
+
+    virtual int getCardsPerCoin(int) = 0;
+    virtual std::string getName() const = 0;
+    virtual void print(std::ostream&) = 0;
+    friend std::ostream& operator <<(std::ostream& os, Card& c);
+};
+
+class Blue: public Card {
+    friend class CardFactory;
+    Blue(): Card((const int[4]){3,6,8,9}){}
+public:
+    int getCardsPerCoin(int) override;
+    std::string getName() const override;
+    void print(std::ostream& os) override;
+};
+
+class Chili: public Card {
+    friend class CardFactory;
+    Chili(): Card((const int[4]){3,6,8,9}){}
+public:
+    int getCardsPerCoin(int) override;
+    std::string getName() const override;
+    void print(std::ostream& os) override;
+};
+
+class Stink: public Card {
+    friend class CardFactory;
+    Stink(): Card((const int[4]){3, 6,8,9}){}
+public:
+    int getCardsPerCoin(int) override;
+    std::string getName() const override;
+    void print(std::ostream& os) override;
+};
+
+class Green: public Card {
+    friend class CardFactory;
+    Green(): Card((const int[4]){3,5,6,7}){}
+public:
+    int getCardsPerCoin(int) override;
+    std::string getName() const override;
+    void print(std::ostream& os) override;
+};
+
+class Soy: public Card {
+    friend class CardFactory;
+    Soy(): Card((const int[4]){2,4,5,6}){}
+public:
+    int getCardsPerCoin(int) override;
+    std::string getName() const override;
+    void print(std::ostream& os) override;
+};
+
+class Black: public Card {
+    friend class CardFactory;
+    Black(): Card((const int[4]){2,4,5,6}){}
+public:
+    int getCardsPerCoin(int) override;
+    std::string getName() const override;
+    void print(std::ostream& os) override;
+};
+
+class Red: public Card {
+    friend class CardFactory;
+    Red(): Card((const int[4]){2,3,4,5}){}
+public:
+    int getCardsPerCoin(int) override;
+    std::string getName() const override;
+    void print(std::ostream& os) override;
+};
+
+class Garden: public Card {
+    friend class CardFactory;
+    Garden(): Card((const int[4]){-1,2,3,-1}){}
+public:
+    int getCardsPerCoin(int) override;
+    std::string getName() const override;
+    void print(std::ostream& os) override;
 };
